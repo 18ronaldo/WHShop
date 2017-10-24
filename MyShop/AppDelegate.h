94d35612c08e6eea5284d7c39c9b2cff0630c4c6
18ronaldo <781815739@qq.com>
2017-10-24 17:45:30 +0800

    创建项目

---
+++ MyShop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyShop
+//
+//  Created by 王辉 on 2017/10/24.
+//  Copyright © 2017年 ronaldo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '8.0'
+target ‘MyShop’ do
+pod 'AFNetworking', '~> 3.1.0'
+pod 'SDWebImage', '~> 4.1.2'
+pod 'Masonry', '~> 1.1.0'
+end

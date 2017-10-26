5c2201bfeaafd7a9fd19bdce3cd036e9b8b99e12
18ronaldo <781815739@qq.com>
2017-10-26 13:56:35 +0800

    简单创建我的（My）顶部view布局

--- MyShop/我的/controller/WHMyViewController.m
+++ MyShop/我的/controller/WHMyViewController.m
@@ -7,8 +7,18 @@
 //
 
 #import "WHMyViewController.h"
+#import <Masonry.h>
 
 @interface WHMyViewController ()
+
+/** 顶部登录和注册的view **/
+@property(nonatomic,strong) UIView *headView;
+/** 顶部背景图片 **/
+@property(nonatomic,strong) UIImageView *headBackImage;
+/** 登录按钮 **/
+@property(nonatomic,strong) UIButton *loginBtn;
+/** 注册按钮 **/
+@property(nonatomic,strong) UIButton *registerBtn;
 
 @end
 
@@ -17,7 +27,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    //设置顶部开始的地方。。。
+    self.edgesForExtendedLayout = 0;
     self.view.backgroundColor = [UIColor orangeColor];
+    
+    [self.view addSubview:self.headView];
+    [self.headView addSubview:self.headBackImage];
+    [self.headView addSubview:self.loginBtn];
+    [self.headView addSubview:self.registerBtn];
+    
+    __weak typeof(self) weakSelf = self;
+    [_headView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.left.right.equalTo(weakSelf.view);
+        make.height.mas_equalTo(150);
+    }];
+    [_headBackImage mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(weakSelf.headView);
+    }];
+    [_loginBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(weakSelf.headView.mas_centerX).offset(-60);
+        make.centerY.equalTo(weakSelf.headView.mas_centerY);
+        make.size.mas_equalTo(CGSizeMake(45, 23));
+    }];
+    [_registerBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(weakSelf.headView.mas_centerX).offset(60);
+        make.centerY.equalTo(weakSelf.headView.mas_centerY);
+        make.size.mas_equalTo(CGSizeMake(45, 23));
+    }];
+    
     // Do any additional setup after loading the view.
 }
 
@@ -26,14 +63,37 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (UIView *)headView{
+    if (!_headView) {
+        _headView = [[UIView alloc] init];
+    }
+    return _headView;
 }
-*/
+- (UIImageView *)headBackImage{
+    if (!_headBackImage) {
+        _headBackImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"我的背景"]];
+    }
+    return _headBackImage;
+}
+- (UIButton *)loginBtn{
+    if (!_loginBtn) {
+        _loginBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_loginBtn setTitle:@"登录" forState:UIControlStateNormal];
+        _loginBtn.titleLabel.font = [UIFont systemFontOfSize:18.0f];
+        _loginBtn.titleLabel.textColor = [UIColor whiteColor];
+    }
+    return _loginBtn;
+}
+- (UIButton *)registerBtn{
+    if (!_registerBtn) {
+        _registerBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_registerBtn setTitle:@"注册" forState:UIControlStateNormal];
+        _registerBtn.titleLabel.font = [UIFont systemFontOfSize:18.0f];
+        _registerBtn.titleLabel.textColor = [UIColor whiteColor];
+    }
+    return _registerBtn;
+}
+
+
 
 @end
